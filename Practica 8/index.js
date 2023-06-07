8c00923a8fb8656519daab3ddf035d0bab6a77a3
GreciaJhoanaMartinez <greciajhoanamartinezcontreras@gmail.com>
2023-06-06 22:55:01 -0600

    Practica 5

---
+++ Practica 8/index.js
@@ -0,0 +1,20 @@
+const searchInput =document.getelementByid('search');
+const results= document.getelementByid('results');
+const searchButton=document.getElementById('Button');
+
+
+searchButton.addEventListener('click',searchGIFs);
+
+function searchGIFs() {
+    const search=searchGIFs.Value;
+
+    return getGipthyResul(url)
+    .then(funtion (gifs))(
+        console.log(gifs)
+        gifs.forEach(function(gif)){
+            const img=document.createElement('img')
+            
+
+        }
+
+    
